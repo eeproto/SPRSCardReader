180be0ef65c9161a154f7060f83845c087b270bb
Christian H <christian@eeproto.com>
2025-01-13 22:11:57 -0500

    find name by card reader

--- cardreader/cardreader.py
+++ cardreader/cardreader.py
@@ -3,6 +3,7 @@ import pygsheets
 from datetime import datetime, timezone
 import json
 
+
 class CardSheet(object):
 
   def __init__(self, settings):
@@ -10,24 +11,39 @@ class CardSheet(object):
     # TODO: json import sheets id
     document = gclient.open_by_key(settings['sheets_id'])
     self.worksheet = document.worksheet_by_title('Active')
-    self.keysheet  = document.worksheet_by_title('Key')
+    self.keysheet = document.worksheet_by_title('Key')
+    self.keysheet_first_column = 4
+    self.keysheet_last_column = 5
+    self.keysheet_card_number_column = 2
 
   def find_row_by_number(self, number):
     cells = self.keysheet.find(number)
     print('found row', cells[0].row)
 
   def entry(self, cardnumber, state):
-    #self.find_row_by_number(cardnumber)
+    # self.find_row_by_number(cardnumber)
     us_timestamp = datetime.now().strftime('%m/%d/%Y-%H:%M:%S')
     iso_timestamp = datetime.now().isoformat()
-    self.worksheet.append_table([iso_timestamp, cardnumber, state], start='A1', end=None, dimension='ROWS', overwrite=False)
+    self.worksheet.append_table([iso_timestamp, cardnumber, state], start='A1', end=None, dimension='ROWS',
+                                overwrite=False)
+
+  def find_cardnumber_by_key(self, site_number, card_number):
+    cells = self.keysheet.find(site_number, matchEntireCell=True)
+    if len(cells) == 0:
+      raise Exception('can not find site number')
+    match_row = None
+    for cell in cells:
+      if self.keysheet.cell((cell.row, self.keysheet_card_number_column)).value == card_number:
+        match_row = cell.row
+    return f'{self.keysheet.cell((match_row, self.keysheet_first_column)).value} {self.keysheet.cell((match_row, self.keysheet_last_column)).value}'
+
 
 class CardListener(object):
 
   def __init__(self, settings):
     self.cardid = ''
     self.settings = settings
-    self.startchar = self.settings['startchar'] 
+    self.startchar = self.settings['startchar']
     self.stopchar_in = self.settings['stopchar_in']
     self.stopchar_out = self.settings['stopchar_out']
     self.collecting = False
@@ -36,10 +52,10 @@ class CardListener(object):
   def convert_card_number(self, card_characters):
     hexcard = int(card_characters, 16)
     if self.settings['hexformat'] == '2:2':
-      facility = (hexcard>>16) & 0xffff
+      facility = (hexcard >> 16) & 0xffff
       card = hexcard & 0xffff
     elif self.settings['hexformat'] == '2:3':
-      facility = (hexcard>>24) & 0xffff
+      facility = (hexcard >> 24) & 0xffff
       card = hexcard & 0xffffff
     else:
       raise Exception('hex format setting unknown')
@@ -50,57 +66,56 @@ class CardListener(object):
   def register(self, card_characters, state):
     combined = self.convert_card_number(card_characters)
     CardSheet(self.settings).entry(combined, state)
-  
+
   def on_press(self, key):
-      try:
-        #print('alphanumeric key {0} pressed'.format(key.char))
-        if key.char == self.stopchar_in:
-          self.collecting = False
-          try:
-            self.register(self.cardid, 'in')
-          except Exception as ex:
-            print('Error decoding card', self.cardid, ex)
-          self.cardid = ''
-        if key.char == self.stopchar_out:
-          self.collecting = False
-          try:
-            self.register(self.cardid, 'out')
-          except Exception as ex:
-            print('Error decoding card', self.cardid, ex)
-          self.cardid = ''
-        if self.collecting:
-          self.cardid += key.char
-        if key.char == self.startchar:
-          self.collecting = True
-        if len(self.cardid)>self.maxlength:
-          self.cardid = ''
-          self.collecting = False
-      except AttributeError:
-          pass
-          #print('special key {0} pressed'.format(key))
-  
-  def on_release(self, key):
+    try:
+      # print('alphanumeric key {0} pressed'.format(key.char))
+      if key.char == self.stopchar_in:
+        self.collecting = False
+        try:
+          self.register(self.cardid, 'in')
+        except Exception as ex:
+          print('Error decoding card', self.cardid, ex)
+        self.cardid = ''
+      if key.char == self.stopchar_out:
+        self.collecting = False
+        try:
+          self.register(self.cardid, 'out')
+        except Exception as ex:
+          print('Error decoding card', self.cardid, ex)
+        self.cardid = ''
+      if self.collecting:
+        self.cardid += key.char
+      if key.char == self.startchar:
+        self.collecting = True
+      if len(self.cardid) > self.maxlength:
+        self.cardid = ''
+        self.collecting = False
+    except AttributeError:
       pass
-      #print('{0} released'.format(key))
-      #if key == keyboard.Key.esc:
-      #    # Stop listener
-      #    return False
+      # print('special key {0} pressed'.format(key))
+
+  def on_release(self, key):
+    pass
+    # print('{0} released'.format(key))
+    # if key == keyboard.Key.esc:
+    #    # Stop listener
+    #    return False
+
 
 if __name__ == '__main__':
-
   with open('settings.json', 'r') as config_file:
     settings = json.load(config_file)
   cards = CardListener(settings)
 
   # Collect events until released
   with keyboard.Listener(
-          on_press=cards.on_press,
-          on_release=cards.on_release) as listener:
-      listener.join()
-  
+      on_press=cards.on_press,
+      on_release=cards.on_release) as listener:
+    listener.join()
+
 #  # ...or, in a non-blocking fashion:
 #  listener = keyboard.Listener(
 #      on_press=on_press,
 #      on_release=on_release)
 #  listener.start()
-
--- test/test_card_listener.py
+++ test/test_card_listener.py
@@ -2,7 +2,7 @@ import unittest
 import datetime
 import json
 from unittest.mock import patch, MagicMock
-from cardreader.cardreader import CardListener
+from cardreader.cardreader import CardListener, CardSheet
 
 
 class CardListenerTester(unittest.TestCase):
@@ -19,3 +19,15 @@ class CardListenerTester(unittest.TestCase):
     self.settings['hexformat'] = '2:3'
     combined = CardListener(self.settings).convert_card_number('1a2b3c4d5e')
     self.assertEqual(combined, '66993951966')
+
+class CardSheetTester(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+    with open('../cardreader/settings-test.json', 'r') as config_file:
+      cls.settings = json.load(config_file)
+
+
+  def test_02_find_a_card_number_in_key_sheet(self):
+    found = CardSheet(self.settings).find_cardnumber_by_key('73','18310')
+    self.assertEqual(found, 'Dylan Condell')
